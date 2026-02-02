9ff9e875e2eb420decb5580307477c7ab026f898
krishna <krischauhan01@gmail.com>
2026-02-02 15:05:22 -0500

    undo changes

--- Labs/Lab_1/Lab_1.c
+++ Labs/Lab_1/Lab_1.c
@@ -3,26 +3,18 @@
 #include <linux/kernel.h>
 #include <linux/sched.h>
 
-static int print_init_pcb(){
-       struct task_struct *task = &init_task;
-       
-       printk(KERN_INFO "init_task fields:\n");
-       printk(KERN_INFO "PID: %d\n", task->pid);
-       printk(KERN_INFO "State: %u\n", task->__state);
-       printk(KERN_INFO "Flags: %u\n", task->flags);
-       printk(KERN_INFO "rt_priority: %d\n", task->rt_priority);
-       printk(KERN_INFO "policy: %u\n", task->policy);
-       printk(KERN_INFO "tgid: %d\n", task->tgid);
-       
-       return 0;
-}
-
 static int Lab_1_init(void)
 {
     struct task_struct *task = &init_task;
 
     printk(KERN_INFO "Loading Lab_1 Module\n");
-    print_init_pcb();
+    printk(KERN_INFO "init_task fields:\n");
+    printk(KERN_INFO "PID: %d\n", task->pid);
+    printk(KERN_INFO "State: %u\n", task->__state);
+    printk(KERN_INFO "Flags: %u\n", task->flags);
+    printk(KERN_INFO "rt_priority: %d\n", task->rt_priority);
+    printk(KERN_INFO "policy: %u\n", task->policy);
+    printk(KERN_INFO "tgid: %d\n", task->tgid);
 
     return 0;
 }
