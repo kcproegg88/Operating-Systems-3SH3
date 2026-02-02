d845a4159999328454efd72bac511e145402eed3
krishna <krischauhan01@gmail.com>
2026-02-01 20:37:00 -0500

    more more

--- Labs/Lab_1/Lab_1.c
+++ Labs/Lab_1/Lab_1.c
@@ -1,39 +1,28 @@
-/**
- * Lab_1.c
- *
- * A simple kernel module. 
- * 
- * To compile, run makefile by entering "make"
- *
- * Operating System Concepts - 10th Edition
- * Copyright John Wiley & Sons - 2018
- */
-
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/sched.h>
 
-/* This function is called when the module is loaded. */
 static int Lab_1_init(void)
 {
-       printk(KERN_INFO "Loading Lab_1 Module\n");
-       struct task_struct *task = &init_task; // Get the current task (process)
-       printk(KERN_INFO "Process List:\n");
-       printk(KERN_INFO "PID: %d\n", task->pid);
-       printk(KERN_INFO "State: %ld\n", task->state);
-       printk(KERN_INFO "Flags: %lu\n", task->flags);
-       printk(KERN_INFO "rt_priority: %d\n", task->rt_priority);
-       printk(KERN_INFO "policy: %u\n", task->policy);
-       printk(KERN_INFO "tgid: %d\n", task->tgid);
-       return 0;
+    struct task_struct *task = &init_task;
+
+    printk(KERN_INFO "Loading Lab_1 Module\n");
+    printk(KERN_INFO "init_task fields:\n");
+    printk(KERN_INFO "PID: %d\n", task->pid);
+    printk(KERN_INFO "State: %ld\n", task->__state);
+    printk(KERN_INFO "Flags: %u\n", task->flags);
+    printk(KERN_INFO "rt_priority: %d\n", task->rt_priority);
+    printk(KERN_INFO "policy: %u\n", task->policy);
+    printk(KERN_INFO "tgid: %d\n", task->tgid);
+
+    return 0;
 }
 
-/* This function is called when the module is removed. */
-static void Lab_1_exit(void) {
-	printk(KERN_INFO "Removing Lab_1 Module\n");
+static void Lab_1_exit(void)
+{
+    printk(KERN_INFO "Removing Lab_1 Module\n");
 }
 
-/* Macros for registering module entry and exit points. */
-module_init( Lab_1_init );
-module_exit( Lab_1_exit );
+module_init(Lab_1_init);
+module_exit(Lab_1_exit);
