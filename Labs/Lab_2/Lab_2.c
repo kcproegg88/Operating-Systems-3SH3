db90b99b935049ea02f95544099b25c6f7a7715f
krishna <krischauhan01@gmail.com>
2026-02-09 14:01:09 -0500

    type shi

---
+++ Labs/Lab_2/Lab_2.c
@@ -0,0 +1,18 @@
+#include <pthread.h>
+#include <stdio.h>
+
+#include <stdlib.h>
+
+int sum; /*globally used among the threads*/
+
+void *runner(void *param); /* thread calls this fxn*/
+
+int main(int argc, char *argv[]){
+    pthread_t tid; /* the thread identifier */
+    pthread_attr_t attr; /* set of attributes for the thread */
+    pthread_attr_init(&attr); /* get the default attributes */
+    pthread_create(&tid,&attr,runner,NULL); /* create the thread */
+    pthread_join(tid,NULL); /* wait for the thread to exit */
+    printf("sum = %d\n",sum);
+    return 0;
+}
