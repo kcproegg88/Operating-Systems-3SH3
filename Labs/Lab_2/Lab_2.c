898c700f4fee453bfda6fe0184caad1560347e60
dougla15-mac <dougla15@mcmaster.ca>
2026-02-09 19:42:15 +0000

    monkey1

--- Labs/Lab_2/Lab_2.c
+++ Labs/Lab_2/Lab_2.c
@@ -3,25 +3,67 @@
 
 #include <stdlib.h>
 
-int sum; /*globally used among the threads*/
+#define NUM_THREADS 6
 
-void *runner(void *param); /* thread calls this fxn*/
+pthread_t threads[NUM_THREADS];
 
-int main(int argc, char *argv[]){
-    pthread_t tid; /* the thread identifier */
-    pthread_attr_t attr; /* set of attributes for the thread */
-    pthread_attr_init(&attr); /* get the default attributes */
-    pthread_create(&tid,&attr,runner,NULL); /* create the thread */
-    pthread_join(tid,NULL); /* wait for the thread to exit */
-    printf("sum = %d\n",sum);
-    return 0;
+
+int amount;
+// void* runner(void * param);
+void* deposit(void * param);
+void* withdraw(void * param);
+
+int main (int argc, char* argv[]){
+    pthread_t tid;
+    pthread_attr_t attr;
+
+    pthread_attr_init(&attr);
+    pthread_create(&tid, &attr, deposit, argv[0]);
+    pthread_create(&tid, &attr, deposit, argv[0]);
+    pthread_create(&tid, &attr, deposit, argv[0]);
+
+    pthread_create(&tid, &attr, withdraw, argv[1]);
+    pthread_create(&tid, &attr, withdraw, argv[1]);
+    pthread_create(&tid, &attr, withdraw, argv[1]);
+
+    int i = 0;
+
+    for (i = 0; i<NUM_THREADS; i++){
+        pthread_join(threads[i], NULL);
+    }
+
+    printf("Final amount = %d\n", amount);
+
 }
 
-void *runner(void *param){
+void * deposit (void * param){
     int i, upper = atoi(param);
-    sum = 0;
-    for(i=1; i<=upper; i++){
-        sum += i;
+
+    for (i = 1; i<=upper; i++){
+        amount +=1;
     }
+    printf("Deposit amount = %d\n", upper);
     pthread_exit(0);
 }
+
+void * withdraw (void * param){
+    int i, upper = atoi(param);
+
+    for (i = 1; i<=upper; i++){
+        amount -=1;
+    }
+    printf("Withdraw amount = %d\n", upper);
+
+    pthread_exit(0);
+}
+
+// void * runner (void * param){
+//     int i, upper = atoi(param);
+//     amount = 0;
+
+//     for (i = 1; i<=upper; i++){
+//         amount +=i;
+//     }
+
+//     pthread_exit(0);
+// }
