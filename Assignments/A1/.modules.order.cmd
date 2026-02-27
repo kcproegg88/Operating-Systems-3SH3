9b74af28e462770f8cc4072b90a6291dbe80fb56
Krishna Chauhan <krischauhan01@gmail.com>
2026-02-27 19:47:04 +0000

    more clean up

--- Assignments/A1/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_modules.order := {   echo seconds.o; :; } > modules.order
--- Assignments/A1/.seconds.mod.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_seconds.mod := printf '%s\n'   seconds.o | awk '!x[$$0]++ { print("./"$$0) }' > seconds.mod
